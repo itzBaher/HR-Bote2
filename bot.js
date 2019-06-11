3186bedb2cc6a70cac0439605d8b4cc0ad4e7e2c
itzBaher <50467099+itzBaher@users.noreply.github.com>
2019-06-12 02:18:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -185,5 +185,15 @@ client.on("message", message => { //clear
 });
 
 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
 
 client.login(process.env.BOT_TOKEN);
